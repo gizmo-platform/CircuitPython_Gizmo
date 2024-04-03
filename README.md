f6bc9c7df39755707cdc92281738ec6d99e19010
Michael Aldridge <aldridge.mac@gmail.com>
2024-04-03 12:12:52 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Gizmo for CircuitPython
+
+This repo contains the CircuitPython library that allows you to
+program your gizmo controller using the CircuitPython environment.
+
+## Prerequisites
+
+You must have installed and configured
+[CircuitPython](https://circuitpython.org/).
---
+++ gizmo.py
@@ -0,0 +1,17 @@
+# SPDX-FileCopyrightText: Copyright (c) 2024 Gizmo Team & Contributors
+#
+# SPDX-License-Identifier: ISC
+"""
+`gizmo`
+================================================================================
+
+Support library for the Gizmo robotics platform.
+
+
+* Author(s): Gizmo Team & Contributors
+"""
+
+# imports
+
+__version__ = "0.0.0+auto.0"
+__repo__ = "https://github.com/gizmo-platform/CircuitPython_Gizmo.git"
